704888807e50f876583840b3bdeca8994a6e767f
assclic19 <anicet93@gmail.com>
2023-02-18 23:34:15 +0100

    sg

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "svr-jenkins" {
     ami = var.AWS_AMI
     instance_type = "t2.micro"
+    vpc_security_group_ids = ["aws_security_group.instance_sg.id"]
     user_data = <<-EOF
           #!/bin/bash
         sudo apt update -y
